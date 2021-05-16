86ce983331ca3c00405ad959281edd45ca40d939
Phuoc Nguyen Thanh <phuocnt0612@gmail.com>
2021-05-16 11:48:01 +0700

    Lite version

--- src/Summernote.jsx
+++ src/Summernote.jsx
@@ -1,7 +1,7 @@
 /* global $ */
 
-import 'summernote/dist/summernote';
-import 'summernote/dist/summernote.css';
+import 'summernote/dist/summernote-lite';
+import 'summernote/dist/summernote-lite.css';
 import 'codemirror/lib/codemirror.css';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
